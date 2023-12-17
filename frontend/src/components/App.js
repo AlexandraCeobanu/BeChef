61101d488cfc70aac0de222b770a105a63071fc5
Alexandra <ceobanu_alexandra@yahoo.com>
2023-12-17 14:24:36 +0200

    create App component

---
+++ frontend/src/components/App.js
@@ -0,0 +1,8 @@
+import { BrowserRouter as Router, Routes,Route} from 'react-router-dom';
+import React from 'react';
+import Login from './Login';
+export default function App(){
+    return(
+        <Login></Login>
+    )
+}
