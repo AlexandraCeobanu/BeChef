9d12198a5ace924490d7c1181cf377d07aee47ed
Alexandra <ceobanu_alexandra@yahoo.com>
2023-12-17 20:53:28 +0200

    create styles for login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,24 +1,29 @@
-
+import '../styles/login.scss';
 export default function Login(){
     return(
         <div className="page">
-            <div className="right-side">
-            <img src="/images/Chef.gif" alt="Chef-gif" />
-            </div>
             <div className="left-side">
+            <img src="/images/Chef2.gif" alt="Chef-gif" />
+            </div>
+            <div className="right-side">
             <div className="title">
-            <img src="/images/hat.svg" alt="Hat" />
+            <img src="/images/hat.svg" alt="Hat" id="hat" />
             <h1>Hello chef</h1>
             </div>
             <form className="form-class">
                 <label for="email">Email</label><br></br>
                 <input type="text" id="email" name="email" required></input><br></br>
 
+                <div id="line1">
                 <label for="password">Password</label>
                 <p>Forgot password?</p>
-               
+                </div>
                 <input type="password" id="password" name="password" required></input>
-                <p>Don't have an account?                  Register</p>
+
+                <div id="line2">
+                <p>Don't have an account?</p>
+                <p>Register</p>
+                </div>
                 <input type="submit" id="submit" name="submit" value="Login"></input>
             </form>
             </div>
