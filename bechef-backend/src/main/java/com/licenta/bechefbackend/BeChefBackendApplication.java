d480d9eeb2421009541fde4b4e81e439d0f5581d
Alexandra <ceobanu_alexandra@yahoo.com>
2024-02-08 15:14:11 +0200

    add backend configurations

---
+++ bechef-backend/src/main/java/com/licenta/bechefbackend/BeChefBackendApplication.java
@@ -0,0 +1,13 @@
+package com.licenta.bechefbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeChefBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeChefBackendApplication.class, args);
+	}
+
+}
---
+++ bechef-backend/src/test/java/com/licenta/bechefbackend/BechefBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licenta.bechefbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BechefBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
