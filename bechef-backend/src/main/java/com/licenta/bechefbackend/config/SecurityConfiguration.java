d7c78553a483e9ab69d24923b92a88368f8add83
Alexandra <ceobanu_alexandra@yahoo.com>
2024-02-22 16:38:30 +0200

    add cors origins

--- bechef-backend/src/main/java/com/licenta/bechefbackend/config/SecurityConfiguration.java
+++ bechef-backend/src/main/java/com/licenta/bechefbackend/config/SecurityConfiguration.java
@@ -14,6 +14,12 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -34,15 +40,29 @@ public class SecurityConfiguration {
 //                    .anyRequest().authenticated()
                             .anyRequest().authenticated()
             )
-                .formLogin(Customizer.withDefaults());
-//                .formLogin(formLogin -> formLogin
-//                .loginPage(Customizer.withDefaults())
-//                .permitAll());
+                .cors(Customizer.withDefaults())
+
+
+                .formLogin(formLogin -> formLogin
+                .loginPage("http://localhost:3000")
+                .permitAll());
 //                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 //                            .authenticationProvider(authenticationProvider()).addFilterBefore(
 //                                    jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class
 //                );
             return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource()
+    {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",configuration);
+        return source;
     }
     @Bean
     public AuthenticationProvider authenticationProvider()
