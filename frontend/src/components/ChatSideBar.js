4562e6432b904080f3c3ae0938a48fe7495c5486
Alexandra <ceobanu_alexandra@yahoo.com>
2024-04-23 12:06:50 +0300

    add styles to sidebar chat

--- frontend/src/components/ChatSideBar.js
+++ frontend/src/components/ChatSideBar.js
@@ -1,9 +1,15 @@
 import TitleSideBar from "./TitleSideBar";
-
+import Comment from "./Comment";
 export default function ChatSideBar() {
     return(
         <div className="sidebar">
             <TitleSideBar></TitleSideBar>
+            <div className="messages">
+            <Comment></Comment>
+            <Comment></Comment>
+            <Comment></Comment>
+            <Comment></Comment>
+            </div>
         </div>
     )
 }
