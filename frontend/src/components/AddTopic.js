d67b76e4f689fcbf848e22eb7fd65fe42e9fe224
Alexandra <ceobanu_alexandra@yahoo.com>
2024-04-23 15:03:31 +0300

    make add topic component

---
+++ frontend/src/components/AddTopic.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+export default function AddTopic(props)
+{
+    const [topic,setTopic] = useState("");
+    const handleValueChange = (event) => {
+        setTopic(event.target.value);
+    }
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+        let thread = {
+            topic: topic,
+            initiatorId: JSON.parse(localStorage.getItem("user")).id
+        }
+          postThread(thread)
+          .then (
+            () => {
+                // if(props.socket!==null)
+                // props.socket.emit("notifyComm", comm)
+                // setComment("");
+                // props.handleCommentAdded();
+            }
+          )
+          .catch((error)=> {
+            setTopic("");
+            console.log(error);
+          })
+        }
+    };
+
+    return(
+        <div className="add-thread">
+            <input type="text" placeholder="Topic / question " value={topic} onChange={handleValueChange} onKeyDown={handleKeyDown}></input>
+        </div>
+    )
+}
--- frontend/src/components/InitiateTopicButton.js
+++ frontend/src/components/InitiateTopicButton.js
@@ -1,9 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faRocketchat } from "@fortawesome/free-brands-svg-icons"
 import "../styles/ThreadChat.scss";
+import { useState } from "react";
 export default function InitiateTopicButton() {
+    const [addTopic, setAddTopic] = useState(false);
+
+    const handleClick = () => {
+        setAddTopic(true);
+    }
     return (
-        <div className="initiate-topic">
+        <div className="initiate-topic" onClick={handleClick}>
             <FontAwesomeIcon icon = {faRocketchat}></FontAwesomeIcon>
             <h5>Initiate Topic</h5>
         </div>
