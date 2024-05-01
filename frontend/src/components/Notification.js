1c4affaf5a95233e7d17421ae6e7341e7586d6e9
Alexandra <ceobanu_alexandra@yahoo.com>
2024-05-01 10:03:24 +0300

    show notification when a message is added style

--- frontend/src/components/Notification.js
+++ frontend/src/components/Notification.js
@@ -3,14 +3,29 @@ import MiniRecipe from "./MiniRecipe"
 import "../styles/notification.scss";
 export default function Notification(props){
     return(
-        <div className = {props.notification.read === false ? "notification read-notification"  : "notification"}>
-            <div className="without-image">
-            <UserBadge userId={props.notification.senderId}></UserBadge>
-            <p>{props.notification.message}</p>
-            </div>
+        <div>
+
             {
-             props.notification.receiverId !==null && 
-            <MiniRecipe recipeId={props.notification.recipeId}></MiniRecipe>
+                props.notification.type !== "message" ? (
+                    <div className = {props.notification.read === false ? "notification read-notification"  : "notification"}>
+                    <div className="without-image">
+                    <UserBadge userId={props.notification.senderId}></UserBadge>
+                    <p>{props.notification.message}</p>
+                    </div>
+                    <MiniRecipe recipeId={props.notification.recipeId}></MiniRecipe>
+                    </div>
+                ) : 
+                (
+                    <div className = {props.notification.read === false ? "notification read-notification"  : "notification"}>
+                    <div className="thread-notification">
+                    <h6>Unde se coc cozonacii?</h6>
+                    <div className="without-image">
+                    <UserBadge userId={props.notification.senderId}></UserBadge>
+                    <p>{props.notification.message}</p>
+                    </div>
+                    </div>
+                    </div>
+                )
             }
         </div>
     )
